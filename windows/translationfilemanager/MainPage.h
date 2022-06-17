305432e2307662664085bf432735d95f2510ab33
Bram Gussekloo <bram.gussekloo@gmail.com>
2022-06-18 02:39:34 +0300

    Add files

---
+++ windows/translationfilemanager/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::translationfilemanager::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::translationfilemanager::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
