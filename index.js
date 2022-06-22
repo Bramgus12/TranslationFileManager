05778fe0637c557922d56f76fe68a8f9fd2fb4c2
Bram Gussekloo <bram.gussekloo@gmail.com>
2022-06-22 03:11:42 +0300

    make reactNative work

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
 import { AppRegistry } from "react-native";
+import { Provider } from "react-native-paper";
 import App from "./src/App";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+function Main() {
+    return (
+        <Provider>
+            <App />
+        </Provider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
